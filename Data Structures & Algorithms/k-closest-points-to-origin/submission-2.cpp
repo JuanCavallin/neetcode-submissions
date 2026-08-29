55a55a48e59684fb65d9c508a34d3083e7d25953
Juan Cavallin <61669937+JuanCavallin@users.noreply.github.com>
2026-08-29 13:29:15 -0400

    Add: k-closest-points-to-origin - submission-2

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        //double eucledian(vector<int>& a, vector<int>& b) {
+        //    return math.sqrt(a[0] - b[0] ** 2 + (a[1] - b[1]) ** 2);
+        //}
+
+        struct Compare {
+            bool operator()(vector<int>& a, vector<int>& b) {
+                return sqrt(pow(a[0], 2) + pow(a[1], 2)) > sqrt(pow(b[0], 2) + pow(b[1], 2));
+            }
+        };
+
+        vector<vector<int>> result;
+        priority_queue<vector<int>, vector<vector<int>>, Compare> minHeap;
+        //Push all the points
+        for(vector<int>& v : points ) {
+            minHeap.push(v);
+        }
+        //Pop k elements and add to result
+        for(int i = 0; i < k; i++) {
+            result.push_back(minHeap.top());
+            minHeap.pop();
+        }
+        return result;
+
+        //TODO: later version where we compare and get the points with the largest distances from each other
+    }
+};
