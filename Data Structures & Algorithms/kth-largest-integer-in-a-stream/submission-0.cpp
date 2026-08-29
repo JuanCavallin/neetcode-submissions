411561e4774c336f6ebbc2b2600786a8e1a88bb1
Juan Cavallin <61669937+JuanCavallin@users.noreply.github.com>
2026-08-29 15:07:25 -0400

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,21 @@
+class KthLargest {
+private:
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    int k;
+public:
+
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(int& n : nums) {
+            this->add(n);
+        }
+    }
+    
+    int add(int val) {
+        minHeap.push(val);
+        if(minHeap.size() > k) {
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+};
