9b76b210d2b5178b9dad19eff1572f7cf8f7284a
Juan Cavallin <61669937+JuanCavallin@users.noreply.github.com>
2026-09-23 03:11:45 -0400

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        for i in range(len(nums)):
+            for j in range(len(result)):
+                result.append(result[j] + [nums[i]])
+        return result #TODO: convert to set
