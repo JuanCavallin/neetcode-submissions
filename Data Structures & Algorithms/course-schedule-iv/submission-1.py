c4239b0dece2830eb9bb2abc394eeaf652049ce4
Juan Cavallin <61669937+JuanCavallin@users.noreply.github.com>
2026-09-24 03:49:38 -0400

    Add: course-schedule-iv - submission-1

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-1.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        graph = defaultdict(list)
+        for start, end in prerequisites:
+            graph[start].append(end)
+        
+        result = []
+
+        def dfs(node, target):
+            if node not in graph:
+                return False #Not a real conditon but sitll leave in 
+            if node == target:
+                return True
+            if node in path:
+                return False
+            path.add(node)
+            for neighbor in graph[node]:
+                if dfs(neighbor, target):
+                    return True
+            path.remove(node)
+            return False
+        
+        for i in range(len(queries)):
+            path = set() #do I actually need for this problem ???? TODO 
+            result.append(dfs(queries[i][0], queries[i][1]))
+        
+        return result
