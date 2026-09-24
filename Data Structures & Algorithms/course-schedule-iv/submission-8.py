8da861d94960ab604b3e0124cf1bf8ef53fb76c3
Juan Cavallin <61669937+JuanCavallin@users.noreply.github.com>
2026-09-24 11:11:32 -0400

    Add: course-schedule-iv - submission-8

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-8.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        graph = defaultdict(list)
+        for start, end in prerequisites:
+            graph[start].append(end)
+        
+        result = []
+        can_reach = defaultdict(set)
+
+        def dfs(node, target, start):
+            if node == target:
+                return True
+            if target in can_reach[node]:
+                return True
+            if node not in graph:
+                return False #Not a real conditon but sitll leave in 
+            if node in path:
+                return False
+            path.add(node)
+            for neighbor in graph[node]:
+                can_reach[start].append(neighbor)
+                if dfs(neighbor, target, start):
+                    return True
+            path.remove(node)
+            return False
+        
+        for i in range(len(queries)):
+            path = set() #do I actually need for this problem ???? TODO
+            if dfs(queries[i][0], queries[i][1], queries[i][0]):
+                result.append(True)
+            else:
+                result.append(False)
+            can_reach[queries[i][0]].append(path)
+        
+        return result
